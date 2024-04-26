c8ab7690a03f0f00a458bc6d0137635427360413
ayanchowdhury242 <134405034+ayanchowdhury242@users.noreply.github.com>
2024-04-26 06:02:08 +0000

    variable declaring

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345;;
+let accountEmail = "ayanchowdhury.gmail.com";
+var accountPass = "ayan12345";
+accountCity = "Kolkata";
+
+//accountId= 2; not allowed [const cannot be changed or reassigned]
+accountEmail = "itsayancool@gmail.com";
+accountPass = "chowdhury123"
+accountCity = "tamluk";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity]);
