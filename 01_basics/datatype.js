f0b5f749888f64a3d4f15182f68cd5162246229d
ayanchowdhury242 <134405034+ayanchowdhury242@users.noreply.github.com>
2024-04-26 06:56:21 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,14 @@
+/*there are 7 types of data types
+1. Null => it is a type of standalone value
+2. Number - 2 to power 53 range.
+3. Boolean
+4. BigInt
+5. Symbols => define uniqueness 
+6. String
+7. Undefined
+*/
+
+let namePerson = "Sumit"; //string
+let age = 24; // number
+let isHeHere = false; //boolean
+console.log(typeof false);
